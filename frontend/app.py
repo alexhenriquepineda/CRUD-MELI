331149efdef21ecfc1e4e6b2e323d07e2a619e71
Alex Pineda <allexhenrique@gmail.com>
2025-02-14 19:34:29 -0300

    feat: add new product and visualize function

--- frontend/app.py
+++ frontend/app.py
@@ -7,3 +7,72 @@ st.set_page_config(layout="wide")
 st.image("logo.png", width=200)
 
 st.title("Gerenciamento de Produtos")
+
+# Função auxiliar para exibir mensagens de erro detalhadas
+def show_response_message(response):
+    if response.status_code == 200:
+        st.success("Operação realizada com sucesso!")
+    else:
+        try:
+            data = response.json()
+            if "detail" in data:
+                # Se o erro for uma lista, extraia as mensagens de cada erro
+                if isinstance(data["detail"], list):
+                    errors = "\n".join([error["msg"] for error in data["detail"]])
+                    st.error(f"Erro: {errors}")
+                else:
+                    # Caso contrário, mostre a mensagem de erro diretamente
+                    st.error(f"Erro: {data['detail']}")
+        except ValueError:
+            st.error("Erro desconhecido. Não foi possível decodificar a resposta.")
+
+# Adicionar Produto
+with st.expander("Adicionar um Novo Produto"):
+    with st.form("new_product"):
+        name = st.text_input("Nome do Produto")
+        description = st.text_area("Descrição do Produto")
+        price = st.number_input("Preço", min_value=0.01, format="%f")
+        categoria = st.selectbox(
+            "Categoria",
+            ["Eletrônico", "Eletrodoméstico", "Móveis", "Roupas", "Calçados"],
+        )
+        email_fornecedor = st.text_input("Email do Fornecedor")
+        submit_button = st.form_submit_button("Adicionar Produto")
+
+        if submit_button:
+            response = requests.post(
+                "http://backend:8000/products/",
+                json={
+                    "name": name,
+                    "description": description,
+                    "price": price,
+                    "categoria": categoria,
+                    "email_fornecedor": email_fornecedor,
+                },
+            )
+            show_response_message(response)
+
+
+with st.expander("Visualizar Produtos"):
+    if st.button("Exibir Todos os Produtos"):
+        response = requests.get("http://backend:8000/products/")
+        if response.status_code == 200:
+            product = response.json()
+            df = pd.DataFrame(product)
+
+            df = df[
+                [
+                    "id",
+                    "name",
+                    "description",
+                    "price",
+                    "category",
+                    "email_supplier",
+                    "created_at",
+                ]
+            ]
+
+            # Exibe o DataFrame sem o índice
+            st.write(df.to_html(index=False), unsafe_allow_html=True)
+        else:
+            show_response_message(response)
